dbf4db8bad787a2dd8b0dc7bb136b0ae32d61e65
AdamWiktor <adam.wiktor97@gmail.com>
2022-09-08 15:36:48 +0200

    ansible

--- copy.sh
+++ copy.sh
@@ -1,3 +1,3 @@
 #!/bin/bash
 
-scp -r src analyzer.dockerfile compose.yaml mvnw mvnw.cmd pom.xml .mvn ansible haproxy kafka setup-analyzer.sh start-analyzer.sh stop-analyzer.sh rozpr:~/analyzer
+scp -r src analyzer.dockerfile compose.yaml mvnw mvnw.cmd pom.xml .mvn ansible haproxy kafka setup-analyzer.sh start-analyzer.sh stop-analyzer.sh rozpr:~/a2/analyzer
